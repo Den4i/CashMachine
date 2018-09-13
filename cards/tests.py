f67c80b1a33853b642ebeeddc88416da4b902abe
Den4i <dglonassik@gmail.com>
2018-09-13 23:36:13 +0300

    using pytest

--- cards/tests.py
+++ cards/tests.py
@@ -1,17 +1,32 @@
-from django.test import TestCase
-from rest_framework.test import APITestCase
 from cards.models import Card
-# Create your tests here.
+import pytest
 
 
-class CardTestCase(APITestCase):
-    def setUp(self):
+@pytest.mark.django_db
+class Test_cardViewSet:
+    def setup(self):
         Card.objects.create(
             number='1234567890123456',
             status='UN',
-            pin='0000'
+            pincode='0000',
+            balance=0
         )
 
-    def test_save_model(self):
-        card_count = Card.objects.count()
-        self.assertEqual(card_count, 1)
+    def test_create(self):
+        Card.objects.create(
+            number='9999999999999999',
+            status='UN',
+            pincode='0000',
+            balance=0
+        )
+        assert Card.objects.count() == 2
+
+    def test_update(self):
+        card = Card.objects.get(number='1234567890123456')
+        card.pincode = '7777'
+        card.save()
+        assert Card.objects.get(number='1234567890123456').pincode == '7777'
+
+    def test_delete(self):
+        Card.objects.get(number='1234567890123456').delete()
+        assert Card.objects.count() == 0
